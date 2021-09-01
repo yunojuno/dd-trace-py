dbd199aff424d820bd61645561ef2b7e846cc705
brettlangdon <brett.langdon@datadoghq.com>
2021-08-31 20:14:45 -0400

    update variable name

--- ddtrace/internal/_c_utils.h
+++ ddtrace/internal/_c_utils.h
@@ -6,18 +6,18 @@ static inline int PyBytesLike_Check(PyObject* o) {
 
 static inline char* PyObject_Copy_Str(PyObject* o) {
   PyObject* temp;
-  char* borrowed;
+  char* copied;
 
   if (PyBytesLike_Check(o)) {
-    borrowed = PyBytes_AS_STRING(o);
-    borrowed = strdup(borrowed);
+    copied = PyBytes_AS_STRING(o);
+    copied = strdup(copied);
   } else if (PyUnicode_Check(o)) {
     temp = PyUnicode_AsEncodedString(o, "utf-8", NULL);
     if (temp == NULL) {
       return NULL;
     }
-    borrowed = PyBytes_AS_STRING(temp);
-    borrowed = strdup(borrowed);
+    copied = PyBytes_AS_STRING(temp);
+    copied = strdup(copied);
     Py_DECREF(temp);
   } else {
     temp = PyObject_Str(o);
@@ -25,10 +25,10 @@ static inline char* PyObject_Copy_Str(PyObject* o) {
       return NULL;
     }
 
-    borrowed = PyBytes_AS_STRING(temp);
-    borrowed = strdup(borrowed);
+    copied = PyBytes_AS_STRING(temp);
+    copied = strdup(copied);
     Py_DECREF(temp);
   }
 
-  return borrowed;
+  return copied;
 }
